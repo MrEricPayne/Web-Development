89b5d4daa41d67404d1b763cc7f5dfa1598665cf
Eric Payne <ecpayne@hotmail.com>
2017-03-20 01:57:09 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+
+
+// -----------------------------------------------------
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The server is active.");
+}); 
